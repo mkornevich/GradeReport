bb43989e5b8300fe8c793e6a38b16402c3ceb182
mkornevich <mkornevich3@gmail.com>
2021-05-26 13:11:58 +0300

    init

---
+++ GradeReport/Group/GroupListForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GradeReport.Group
+{
+    public partial class GroupListForm : Form
+    {
+        public GroupListForm()
+        {
+            InitializeComponent();
+        }
+
+        private void GroupListForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ GradeReport/MainForm.Designer.cs
@@ -0,0 +1,303 @@
+﻿
+namespace GradeReport
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.справочникиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.отчетыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.группыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.курсыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.семестрыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.периодыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.квалификацииToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.специальностиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.студентыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.предметыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.успеваемостьЗаПериодToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.успеваемостьКРодительскомуСобраниюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ведомостьОтметокПоКурсовомуПроектуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ведомостьОтметокЗаСеместрToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.анализРезультатовОКРКПиЯПToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.анализРезультатовДКРКПиЯПToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.экзаменационнаяВедомостьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.отчетУспеваемостиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.мониторингУчебныхДостиженийГруппыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.своднаяВедомостьУспеваемостиУчащихсяГруппыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.своднаяВедомостьУспеваемостиСлужебнаяЗапискаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.своднаяВедомостьЗаПериодОбученияКрасныйДипломToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.справочникиToolStripMenuItem,
+            this.отчетыToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(1440, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // справочникиToolStripMenuItem
+            // 
+            this.справочникиToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.группыToolStripMenuItem,
+            this.курсыToolStripMenuItem,
+            this.семестрыToolStripMenuItem,
+            this.периодыToolStripMenuItem,
+            this.квалификацииToolStripMenuItem,
+            this.специальностиToolStripMenuItem,
+            this.студентыToolStripMenuItem,
+            this.предметыToolStripMenuItem});
+            this.справочникиToolStripMenuItem.Name = "справочникиToolStripMenuItem";
+            this.справочникиToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.справочникиToolStripMenuItem.Text = "Справочники";
+            // 
+            // отчетыToolStripMenuItem
+            // 
+            this.отчетыToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.успеваемостьЗаПериодToolStripMenuItem,
+            this.успеваемостьКРодительскомуСобраниюToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.ведомостьОтметокПоКурсовомуПроектуToolStripMenuItem,
+            this.ведомостьОтметокЗаСеместрToolStripMenuItem,
+            this.анализРезультатовОКРКПиЯПToolStripMenuItem,
+            this.анализРезультатовДКРКПиЯПToolStripMenuItem,
+            this.экзаменационнаяВедомостьToolStripMenuItem,
+            this.отчетУспеваемостиToolStripMenuItem,
+            this.мониторингУчебныхДостиженийГруппыToolStripMenuItem,
+            this.своднаяВедомостьУспеваемостиУчащихсяГруппыToolStripMenuItem,
+            this.своднаяВедомостьУспеваемостиСлужебнаяЗапискаToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.своднаяВедомостьЗаПериодОбученияКрасныйДипломToolStripMenuItem});
+            this.отчетыToolStripMenuItem.Name = "отчетыToolStripMenuItem";
+            this.отчетыToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.отчетыToolStripMenuItem.Text = "Отчеты";
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            // 
+            // группыToolStripMenuItem
+            // 
+            this.группыToolStripMenuItem.Name = "группыToolStripMenuItem";
+            this.группыToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.группыToolStripMenuItem.Text = "Группы";
+            this.группыToolStripMenuItem.Click += new System.EventHandler(this.группыToolStripMenuItem_Click);
+            // 
+            // курсыToolStripMenuItem
+            // 
+            this.курсыToolStripMenuItem.Name = "курсыToolStripMenuItem";
+            this.курсыToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.курсыToolStripMenuItem.Text = "Курсы";
+            // 
+            // семестрыToolStripMenuItem
+            // 
+            this.семестрыToolStripMenuItem.Name = "семестрыToolStripMenuItem";
+            this.семестрыToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.семестрыToolStripMenuItem.Text = "Семестры";
+            // 
+            // периодыToolStripMenuItem
+            // 
+            this.периодыToolStripMenuItem.Name = "периодыToolStripMenuItem";
+            this.периодыToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.периодыToolStripMenuItem.Text = "Периоды";
+            // 
+            // квалификацииToolStripMenuItem
+            // 
+            this.квалификацииToolStripMenuItem.Name = "квалификацииToolStripMenuItem";
+            this.квалификацииToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.квалификацииToolStripMenuItem.Text = "Квалификации";
+            // 
+            // специальностиToolStripMenuItem
+            // 
+            this.специальностиToolStripMenuItem.Name = "специальностиToolStripMenuItem";
+            this.специальностиToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.специальностиToolStripMenuItem.Text = "Специальности";
+            // 
+            // студентыToolStripMenuItem
+            // 
+            this.студентыToolStripMenuItem.Name = "студентыToolStripMenuItem";
+            this.студентыToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.студентыToolStripMenuItem.Text = "Студенты";
+            // 
+            // предметыToolStripMenuItem
+            // 
+            this.предметыToolStripMenuItem.Name = "предметыToolStripMenuItem";
+            this.предметыToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.предметыToolStripMenuItem.Text = "Предметы";
+            // 
+            // успеваемостьЗаПериодToolStripMenuItem
+            // 
+            this.успеваемостьЗаПериодToolStripMenuItem.Name = "успеваемостьЗаПериодToolStripMenuItem";
+            this.успеваемостьЗаПериодToolStripMenuItem.Size = new System.Drawing.Size(418, 22);
+            this.успеваемостьЗаПериодToolStripMenuItem.Text = "1.1 Успеваемость за период";
+            // 
+            // успеваемостьКРодительскомуСобраниюToolStripMenuItem
+            // 
+            this.успеваемостьКРодительскомуСобраниюToolStripMenuItem.Name = "успеваемостьКРодительскомуСобраниюToolStripMenuItem";
+            this.успеваемостьКРодительскомуСобраниюToolStripMenuItem.Size = new System.Drawing.Size(418, 22);
+            this.успеваемостьКРодительскомуСобраниюToolStripMenuItem.Text = "1.2 Успеваемость к родительскому собранию";
+            // 
+            // ведомостьОтметокПоКурсовомуПроектуToolStripMenuItem
+            // 
+            this.ведомостьОтметокПоКурсовомуПроектуToolStripMenuItem.Name = "ведомостьОтметокПоКурсовомуПроектуToolStripMenuItem";
+            this.ведомостьОтметокПоКурсовомуПроектуToolStripMenuItem.Size = new System.Drawing.Size(418, 22);
+            this.ведомостьОтметокПоКурсовомуПроектуToolStripMenuItem.Text = "2.1 Ведомость отметок по курсовому проекту";
+            // 
+            // ведомостьОтметокЗаСеместрToolStripMenuItem
+            // 
+            this.ведомостьОтметокЗаСеместрToolStripMenuItem.Name = "ведомостьОтметокЗаСеместрToolStripMenuItem";
+            this.ведомостьОтметокЗаСеместрToolStripMenuItem.Size = new System.Drawing.Size(418, 22);
+            this.ведомостьОтметокЗаСеместрToolStripMenuItem.Text = "2.2 Ведомость отметок за семестр";
+            // 
+            // анализРезультатовОКРКПиЯПToolStripMenuItem
+            // 
+            this.анализРезультатовОКРКПиЯПToolStripMenuItem.Name = "анализРезультатовОКРКПиЯПToolStripMenuItem";
+            this.анализРезультатовОКРКПиЯПToolStripMenuItem.Size = new System.Drawing.Size(418, 22);
+            this.анализРезультатовОКРКПиЯПToolStripMenuItem.Text = "2.3 Анализ результатов ОКР (КПиЯП)";
+            // 
+            // анализРезультатовДКРКПиЯПToolStripMenuItem
+            // 
+            this.анализРезультатовДКРКПиЯПToolStripMenuItem.Name = "анализРезультатовДКРКПиЯПToolStripMenuItem";
+            this.анализРезультатовДКРКПиЯПToolStripMenuItem.Size = new System.Drawing.Size(418, 22);
+            this.анализРезультатовДКРКПиЯПToolStripMenuItem.Text = "2.3.5 Анализ результатов ДКР (КПиЯП)";
+            // 
+            // экзаменационнаяВедомостьToolStripMenuItem
+            // 
+            this.экзаменационнаяВедомостьToolStripMenuItem.Name = "экзаменационнаяВедомостьToolStripMenuItem";
+            this.экзаменационнаяВедомостьToolStripMenuItem.Size = new System.Drawing.Size(418, 22);
+            this.экзаменационнаяВедомостьToolStripMenuItem.Text = "2.4 Экзаменационная ведомость";
+            // 
+            // отчетУспеваемостиToolStripMenuItem
+            // 
+            this.отчетУспеваемостиToolStripMenuItem.Name = "отчетУспеваемостиToolStripMenuItem";
+            this.отчетУспеваемостиToolStripMenuItem.Size = new System.Drawing.Size(418, 22);
+            this.отчетУспеваемостиToolStripMenuItem.Text = "2.5 Отчет успеваемости";
+            // 
+            // мониторингУчебныхДостиженийГруппыToolStripMenuItem
+            // 
+            this.мониторингУчебныхДостиженийГруппыToolStripMenuItem.Name = "мониторингУчебныхДостиженийГруппыToolStripMenuItem";
+            this.мониторингУчебныхДостиженийГруппыToolStripMenuItem.Size = new System.Drawing.Size(418, 22);
+            this.мониторингУчебныхДостиженийГруппыToolStripMenuItem.Text = "2.7 Мониторинг учебных достижений группы";
+            // 
+            // своднаяВедомостьУспеваемостиУчащихсяГруппыToolStripMenuItem
+            // 
+            this.своднаяВедомостьУспеваемостиУчащихсяГруппыToolStripMenuItem.Name = "своднаяВедомостьУспеваемостиУчащихсяГруппыToolStripMenuItem";
+            this.своднаяВедомостьУспеваемостиУчащихсяГруппыToolStripMenuItem.Size = new System.Drawing.Size(418, 22);
+            this.своднаяВедомостьУспеваемостиУчащихсяГруппыToolStripMenuItem.Text = "2.8 Сводная ведомость успеваемости учащихся группы";
+            // 
+            // своднаяВедомостьУспеваемостиСлужебнаяЗапискаToolStripMenuItem
+            // 
+            this.своднаяВедомостьУспеваемостиСлужебнаяЗапискаToolStripMenuItem.Name = "своднаяВедомостьУспеваемостиСлужебнаяЗапискаToolStripMenuItem";
+            this.своднаяВедомостьУспеваемостиСлужебнаяЗапискаToolStripMenuItem.Size = new System.Drawing.Size(418, 22);
+            this.своднаяВедомостьУспеваемостиСлужебнаяЗапискаToolStripMenuItem.Text = "2.9 Сводная ведомость успеваемости (Служебная записка)";
+            // 
+            // своднаяВедомостьЗаПериодОбученияКрасныйДипломToolStripMenuItem
+            // 
+            this.своднаяВедомостьЗаПериодОбученияКрасныйДипломToolStripMenuItem.Name = "своднаяВедомостьЗаПериодОбученияКрасныйДипломToolStripMenuItem";
+            this.своднаяВедомостьЗаПериодОбученияКрасныйДипломToolStripMenuItem.Size = new System.Drawing.Size(418, 22);
+            this.своднаяВедомостьЗаПериодОбученияКрасныйДипломToolStripMenuItem.Text = "4.1 Сводная ведомость за период обучения (Красный диплом)";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(415, 6);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(415, 6);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1440, 764);
+            this.Controls.Add(this.menuStrip);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "MainForm";
+            this.Text = "GradeReport";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem справочникиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem отчетыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem группыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem курсыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem семестрыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem периодыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem квалификацииToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem специальностиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem студентыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem предметыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem успеваемостьЗаПериодToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem успеваемостьКРодительскомуСобраниюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem ведомостьОтметокПоКурсовомуПроектуToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ведомостьОтметокЗаСеместрToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem анализРезультатовОКРКПиЯПToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem анализРезультатовДКРКПиЯПToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem экзаменационнаяВедомостьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem отчетУспеваемостиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem мониторингУчебныхДостиженийГруппыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem своднаяВедомостьУспеваемостиУчащихсяГруппыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem своднаяВедомостьУспеваемостиСлужебнаяЗапискаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem своднаяВедомостьЗаПериодОбученияКрасныйДипломToolStripMenuItem;
+    }
+}
+
