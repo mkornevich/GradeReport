778d0ee6f1f063918b2912450b1f18ad6d0825aa
mkornevich <mkornevich3@gmail.com>
2021-07-15 16:44:15 +0300

    check project model.

---
+++ Tree.py
@@ -0,0 +1,43 @@
+Проект - ProjectNode
+	Конфигурация - ConfigNode
+	Предметы - SubjectListNode
+		Предмет - SubjectNode
+			Мои студенты - MyStudentInSubjectRefListNode
+				Студент - MyStudentInSubjectRefNode
+	Квалификации - QualificationListNode
+		Квалификация - QualificationNode
+			Специальности - SpecialtyListNode
+				Специальность - SpecialtyNode
+	Группы - GroupListNode
+		Группа - GroupNode
+			Студетны - StudentListNode
+				Студент - StudentNode
+			Предметы - GroupSubjectRefListNode
+				Предмет - GroupSubjectRefNode
+			Курсы - CourseListNode
+				Курс - CourseNode
+					Семестры - SemesterListNode
+						Семестр - SemesterNode
+							Студенты - SemesterStudentRefListNode
+								Студент - SemesterStudentRefNode
+							Предметы - SemesterSubjectRefListNode
+								Предмет - SemesterSubjectRefNode
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			
